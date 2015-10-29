361098667e91e4ec9eb08633f0b825ecb03c4254
Luca Stefani <luca.stefani.ge1@gmail.com>
2015-10-29 10:02:38 -0700

    ferrari: remove custom mkbootimg
    Change-Id: I71b2c8b9ce022d80ecce791d1fe4b8a71d719619

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,10 +65,10 @@ TARGET_CPU_CORTEX_A53 := true
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
 
 # Kernel
-BOARD_CUSTOM_BOOTIMG_MK            := $(LOCAL_PATH)/mkbootimg.mk
 BOARD_MKBOOTIMG_ARGS               := --ramdisk_offset 0x02000000 --tags_offset 0x01E00000
 BOARD_KERNEL_CMDLINE               := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
 BOARD_KERNEL_SEPARATED_DT          := true
+BOARD_DTBTOOL_ARGS                 := -2
 BOARD_KERNEL_BASE                  := 0x80000000
 BOARD_KERNEL_PAGESIZE              := 2048
 BOARD_KERNEL_TAGS_OFFSET           := 0x01E00000
